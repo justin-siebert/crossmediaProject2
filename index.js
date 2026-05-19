1dbfc0f5c6258a5a1fa4e463945d975cf5c75e1e
Justin Siebert <19justinsiebert99@gmail.com>
2026-05-19 15:24:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const locations = {
     end: { lat: 55.610636, lon: 12.974856, key: "endUnlocked", name: "Gängträffen" }
 };
 
-const THRESHOLD = 50; 
+const THRESHOLD = 40; 
 
 document.addEventListener("DOMContentLoaded", () => {
     updateMenu();
